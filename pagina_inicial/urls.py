43545a0b45a64eae07c04c56f5187888aeef3e38
Janiel Maia <janielmaia2023@gmail.com>
2025-02-28 21:58:47 -0300

    feat(app:pagina_inicial) - view e template iniciais

---
+++ pagina_inicial/urls.py
@@ -0,0 +1,6 @@
+from django.urls import path
+from .views import *
+
+urlpatterns = [
+    path('', PaginaInicialView.as_view(), name='pagina-inicial'),
+]
--- sitefsspe/settings.py
+++ sitefsspe/settings.py
@@ -37,6 +37,8 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
+    # APPS DO PROJETO
+    "pagina_inicial",
 ]
 
 MIDDLEWARE = [
